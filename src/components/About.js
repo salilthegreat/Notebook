62e44be50cb8046ecaa9247ec346f74c2efb25e8
salilthegreat <salilkumardas22@gmail.com>
2023-04-13 11:20:52 -0700

    Navbar and Home updated

--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,8 @@ import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import NoteContext from '../context/notes/noteContext'
 const About = () => {
-  // Assigning the variable with the NoteContext value
-  const a = useContext(NoteContext)
-  //useEffect hook is used to run the function first time it works like componentDidMount
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  },[])
   return (
-    //Accessing the value using the variable
-    // <div>About me : I am {a.name}, studying in class {a.class}</div>
-    //Accesing the value from the object as both state and update function is passed
-    <div>About me : I am {a.state.name}, studying in class {a.state.class}</div>
+    <div>About me </div>
   )
 }
 
--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -1,30 +1,9 @@
 import NoteContext from "./noteContext";
 import { useState } from 'react'
 const NoteState = (props)=>{
-    //s1 is a state
-    let s1 = {
-        "name" : "Salil",
-        "class" : "10b",
-    }
-    //useState is used
-    const [state,setState] = useState(s1)
-    //State is changed using this function
-    const update= () => {
-        setTimeout(()=>{
-            setState({
-                "name" : "Bibek",
-                "class" : "11A",
-            })
-        },1000);
-    }
 return(
-    //Below code is used to send the values of the state
-    // <NoteContext.Provider value = {s1}>
-    // {props.children}
-    // </NoteContext.Provider>
 
-    //Here we are sending state with a function which will change it
-        <NoteContext.Provider value = {{state,update}}>
+        <NoteContext.Provider value = {{}}>
         {props.children}
         </NoteContext.Provider>
     )
