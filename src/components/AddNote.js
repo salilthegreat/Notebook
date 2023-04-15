4f013feb54b3854d5e677fb709e627bf9edba8e8
salilthegreat <salilkumardas22@gmail.com>
2023-04-15 11:24:33 -0700

    now Note can be added at the backend

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -4,17 +4,21 @@ export const AddNote = () => {
     const context = useContext(NoteContext);
     const {addNote} = context;
     //a state is created to feed the values in to the addNote() from the inputs
-    const [note,setNote] = useState ({title:" ",description:" ",tag:""})
+    // const [note,setNote] = useState ({title:" ",description:" ",tag:" "})
+    const [note,setNote] = useState ({title:"",description:"",tag:""})
     //Function to fetch the input values
     const onChange = (e) =>{
     //it says change nothing from the note except the change in these input and set it's value to the value of input provided by user
-        setNote({...note,[e.target.name]:e.target.value})
+        setNote({...note,[e.target.name]:e.target.value});
+
     }
     //On click of button
     const handleCLicked = (e)=>{
         e.preventDefault();
     //values fetched from the inputs are given to the addNote() which will use them to create a new note
-        addNote(note.title,note.description,note.tag)
+    // console.log(note.title);
+        addNote(note.title,note.description,note.tag);
+        setNote({title:"",description:"",tag:""})
     }
     return (
         <div className="container">
@@ -24,15 +28,15 @@ export const AddNote = () => {
                     <form>
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title </label>
-                            <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" onChange={onChange}/>
+                            <input type="text"  className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label">Description</label>
-                            <input type="text" className="form-control" id="description" name='description' onChange={onChange}/>
+                            <input type="text"  className="form-control" id="description" name="description" onChange={onChange}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="tag" className="form-label">Tag</label>
-                            <input type="text" className="form-control" id="tag" name='tag' onChange={onChange}/>
+                            <input type="text"  className="form-control" id="tag" name="tag" onChange={onChange}/>
                         </div>
 
                         <button type="submit" className="btn btn-primary" onClick={handleCLicked}>Add Note</button>
--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -28,64 +28,65 @@ const NoteState = (props) => {
   }
 
 
-//Add a Note
-  const addNote = ( title,description,tag)=>{
-    
-    //note is hardcoded but later it will be added by api
-    const note = {
-      "_id": "6435e3bcd23bbfaeddb4eff7",
-      "user": "64326219018bffe34d481349",
-      "title": title,
-      "description": description,
-      "tag": "Fiery Resolve",
-      "__v": 0
-    }
+  //Add a Note
+  const addNote = async (title,description,tag) => {
+    //API call
+    const response = await fetch(`${host}/api/notes/addnote`,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQzMjYyMTkwMThiZmZlMzRkNDgxMzQxIn0sImlhdCI6MTY4MTI0NDg1N30.YH9ipmOC60IPSiDTJzaw9zRZivXUnXOdYVl-8aoe5Oo"
+      },
+      body: JSON.stringify({title,description,tag})
+    })
+    const json = await response.json();
+    //new note added to the list
+    setNotes(notes.concat(json))
+  }
 
-    //to add notes to the list of notes already present
-  setNotes(notes.concat(note))
-}
+  //Edit a Notes
+  const editNote = (id, title, description, tag) => {
+    //API call
 
-//Edit a Notes
-const editNote = (id, title, description, tag) => {
-  //API call
-
-  //each note is fetched from the note array
-  for (let i = 0; i < notes.length; i++) {
-    const element = notes[i];
-    //The element whose id matches that of the id to be edited will be edited
-    if (element._id === id) {
-      element.title = title;
-      element.description = description;
-      element.tag = tag;
+    //each note is fetched from the note array
+    for (let i = 0; i < notes.length; i++) {
+      const element = notes[i];
+      //The element whose id matches that of the id to be edited will be edited
+      if (element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
+      }
     }
   }
-}
 
-//Delete Note
-//it takes id as input
-const deleteNote = async (id) => {
-  const response = await fetch(`${host}/api/notes/deletenote/${id}`,{
-    method: "DELETE",
-    headers:{
-      "Content-Type":"application/json",
-      "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQzMjYyMTkwMThiZmZlMzRkNDgxMzQxIn0sImlhdCI6MTY4MTI0NDg1N30.YH9ipmOC60IPSiDTJzaw9zRZivXUnXOdYVl-8aoe5Oo"
-    }
-  })
-  const json = await response.json()
-  console.log(json)
-  console.log("deleting the note with id:" + id)
-  //new notes are stored by filtering the notes and storing the one which don't have the given id 
-  const newNotes = notes.filter((note) => { return note._id !== id })
-  setNotes(newNotes)
-}
+  //Delete Note
+  //it takes id as input
+  const deleteNote = async (id) => {
+    //API call
+    //id of the note is provided which will be  fetched from Noteitem.js
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQzMjYyMTkwMThiZmZlMzRkNDgxMzQxIn0sImlhdCI6MTY4MTI0NDg1N30.YH9ipmOC60IPSiDTJzaw9zRZivXUnXOdYVl-8aoe5Oo"
+      }
+    })
+    const json = await response.json()
+    console.log(json)
+    console.log("deleting the note with id:" + id)
+    //new notes are stored by filtering the notes and storing the one which don't have the given id 
+    const newNotes = notes.filter((note) => { return note._id !== id })
+    setNotes(newNotes)
+  }
 
 
-return (
-  //passing the state and different functions
-  <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote,getNotes }}>
-    {props.children}
-  </NoteContext.Provider>
-)
+  return (
+    //passing the state and different functions
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
 
 }
 export default NoteState
