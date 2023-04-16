e88c23b63f5deb02b5c5f072f777b33c2937976b
salilthegreat <salilkumardas22@gmail.com>
2023-04-15 19:08:39 -0700

    User Interface Improoved

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,24 +1,25 @@
-import React,{ useContext,useState } from 'react';
+import React, { useContext, useState } from 'react';
 import NoteContext from '../context/notes/noteContext';
 export const AddNote = () => {
     const context = useContext(NoteContext);
-    const {addNote} = context;
+    const { addNote } = context;
     //a state is created to feed the values in to the addNote() from the inputs
     // const [note,setNote] = useState ({title:" ",description:" ",tag:" "})
-    const [note,setNote] = useState ({title:"",description:"",tag:""})
+    const [note, setNote] = useState({ title: "", description: "", tag: "" })
     //Function to fetch the input values
-    const onChange = (e) =>{
-    //it says change nothing from the note except the change in these input and set it's value to the value of input provided by user
-        setNote({...note,[e.target.name]:e.target.value});
+    const onChange = (e) => {
+        //it says change nothing from the note except the change in these input and set it's value to the value of input provided by user
+        setNote({ ...note, [e.target.name]: e.target.value });
 
     }
     //On click of button
-    const handleClicked = (e)=>{
+    const handleClicked = (e) => {
         e.preventDefault();
-    //values fetched from the inputs are given to the addNote() which will use them to create a new note
-    // console.log(note.title);
-        addNote(note.title,note.description,note.tag);
-        setNote({title:"",description:"",tag:""})
+        //values fetched from the inputs are given to the addNote() which will use them to create a new note
+        // console.log(note.title);
+        addNote(note.title, note.description, note.tag);
+        //values are revert back to normal so that the input can be supplied with these values and become empty when add button clicked
+        setNote({ title: "", description: "", tag: "" })
     }
     return (
         <div className="container">
@@ -28,18 +29,18 @@ export const AddNote = () => {
                     <form>
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title </label>
-                            <input type="text"  className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
+                            <input type="text" value={note.title} className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label">Description</label>
-                            <input type="text"  className="form-control" id="description" name="description" onChange={onChange} />
+                            <input type="text" value={note.description} className="form-control" id="description" name="description" onChange={onChange} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="tag" className="form-label">Tag</label>
-                            <input type="text"  className="form-control" id="tag" name="tag" onChange={onChange}/>
+                            <input type="text" value={note.tag} className="form-control" id="tag" name="tag" onChange={onChange} />
                         </div>
-
-                        <button  type="submit" className="btn btn-primary" onClick={handleClicked}>Add Note</button>
+                        {/* min length propert can't be set bcz the button is'nt submit and the inputs have onChange,using disabled we set the minimum limit for each element */}
+                        <button disabled={note.title.length < 5 || note.description.length < 5 || note.tag.length < 5} type="submit" className="btn btn-primary" onClick={handleClicked}>Add Note</button>
                     </form>
                 </div>
             </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -7,7 +7,7 @@ export const Notes = () => {
     //importing and using the noteContext
     const context = useContext(NoteContext);
     //destructurint the values from the context file 
-    const { notes, getNotes,editNote } = context;
+    const { notes, getNotes, editNote } = context;
     //ureference variable declared
     const ref = useRef(null)
     const closeRef = useRef(null)
@@ -31,7 +31,7 @@ export const Notes = () => {
     }
     const handleClicked = (e) => {
         e.preventDefault();
-        editNote(note.id,note.etitle,note.edescription,note.etag);
+        editNote(note.id, note.etitle, note.edescription, note.etag);
         //ref use to close the modal when update note is clicked
         closeRef.current.click()
     }
@@ -64,20 +64,25 @@ export const Notes = () => {
                                     <input type="text" value={note.edescription} className="form-control" id="edescription" name="edescription" onChange={onChange} />
                                 </div>
                                 <div className="mb-3">
-                                    <label  htmlFor="etag" className="form-label">Tag</label>
-                                    <input type="text" value={note.etag} className="form-control" id="etag" name="etag" onChange={onChange} />
+                                    <label htmlFor="etag" className="form-label">Tag</label>
+                                    <input  type="text" value={note.etag} className="form-control" id="etag" name="etag" onChange={onChange} />
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button ref={closeRef} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button onClick={handleClicked} type="button" className="btn btn-primary">Update Note</button>
+                            <button disabled={note.etitle.length < 5 || note.edescription.length < 5 || note.etag.length < 5} onClick={handleClicked} type="button" className="btn btn-primary">Update Note</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="container">
                 <h2 className="my-3 me-2">Your Notes</h2>
+                <div className="container m-auto">
+                    {/* when no notes are available it will show this as message */}
+                    {notes.length === 0 && "No notes to display"}
+                </div>
+
                 {/* Iteerating every element from the notes */}
                 <div className="row">
                     {notes.map((note) => {
